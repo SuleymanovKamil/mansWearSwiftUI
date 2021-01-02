6b7b194e1729c3c5c0d35977d0ab204497d15fbf
Kamil <rehaurd@mail.ru>
2021-01-02 19:16:39 +0300

    Initial Commit

---
+++ mrKingsman/mrKingsmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  mrKingsmanApp.swift
+//  mrKingsman
+//
+//  Created by Камиль  Сулейманов on 02.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct mrKingsmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
